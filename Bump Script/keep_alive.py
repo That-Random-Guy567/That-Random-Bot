44b942b19cc46a4d0bd44675f1e152f67a305d1c
That Random Coder <172260140+That-RandomCoder@users.noreply.github.com>
2025-04-16 11:30:02 +0800

    first commit

---
+++ Bump Script/keep_alive.py
@@ -0,0 +1,17 @@
+"""
+from threading import Thread
+from flask import Flask    
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "bot is working hopefully"
+
+def run():
+    app.run(host="0.0.0.0", port=5000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+"""
---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord_Bot
+Making custom discord bot
+idk what to put here
